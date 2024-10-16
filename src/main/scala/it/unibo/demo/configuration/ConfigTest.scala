9a1ef9f4d5a28572d1397b30df295400b7875bee
noodles <elvis.perlika@studio.unibo.it>
2024-10-16 09:56:06 +0200

    feat: add simple Aggregate program to test rotation and update javaFX versione

--- src/main/scala/it/unibo/demo/configuration/ConfigTest.scala
+++ src/main/scala/it/unibo/demo/configuration/ConfigTest.scala
@@ -1,17 +1,26 @@
 package it.unibo.demo.configuration
 
 import it.unibo.demo.robot.WaveRobot
+import upickle.default._
+
 import scala.collection.mutable.ListBuffer
 
-object configTest {
+object ConfigTest {
   def main(args: Array[String]): Unit = {
     val jsonString = os.read(os.pwd / "src"/"main"/"scala"/"it"/"unibo"/"demo"/"configuration"/"waveRobot.json")
     val data = ujson.read(jsonString)
     // create an empty list named robots
+    println(data)
+    val waveRobot: WaveRobot = read[WaveRobot]("""{"ip": "192.168.8.10","id": 6}""")
+    val waveRobots = read[Seq[WaveRobot]](data)
+    waveRobots.foreach(println)
+    val list = read[Seq[WaveRobot]](data)
+    println(list)
+    /*
     var robots = ListBuffer[WaveRobot]()
     data.arr.map(i => {
       println(i("ip").str + " " + i("value").num.toInt)
       robots += WaveRobot(i("ip").str, i("value").num.toInt)
-    })
+    }) */
   }
 }
